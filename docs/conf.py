a1db6a3f4e4dfaf04ee6980af77e3beb91c3f704
NickSwainston <nickaswainston@gmail.com>
2023-07-20 09:47:00 +0800

    Removed unused extensions

--- docs/conf.py
+++ docs/conf.py
@@ -20,11 +20,7 @@ author = 'Nick Swainston'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'numpydoc',
